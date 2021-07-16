d227bd751ef62e8767e2fccd7ab1bfd2acf8ad87
allenwork <allenwork2021@163.com>
2021-07-16 17:26:16 +0800

    spring mvc学习（图灵） 2021-0716

--- spring-study/spring-ioc2/src/main/java/com/study/SpringTest.java
+++ spring-study/spring-ioc2/src/main/java/com/study/SpringTest.java
@@ -2,13 +2,19 @@ package com.study;
 
 import com.study.entity.DriverFactoryBean;
 import com.study.entity.HelloSpring;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
 
 import java.sql.Driver;
 
 public class SpringTest {
 
     public static void main(String[] args) {
+        // 加载文件系统中配置文件
+//        ApplicationContext context =
+//                new FileSystemXmlApplicationContext("C:\\Users\\86131\\IdeaProjects\\study-project-java\\spring-study\\spring-ioc2\\src\\main\\resources\\spring.xml");
+        // 加载工程 classpath 下的配置文件实例化
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
         HelloSpring helloSpring = (HelloSpring) context.getBean("springHello");
         DriverFactoryBean driverFactoryBean = context.getBean(DriverFactoryBean.class);
