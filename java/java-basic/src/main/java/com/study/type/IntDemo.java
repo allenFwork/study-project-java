fe3035b33c00f1b356445c9785e7b5b33f34f7d7
allenwork <allenwork2021@163.com>
2021-08-19 16:49:37 +0800

    java 基础功能学习 2021-08-19

---
+++ java/java-basic/src/main/java/com/study/type/IntDemo.java
@@ -0,0 +1,10 @@
+package com.study.type;
+
+public class IntDemo {
+
+    public static void main(String[] args) {
+        int i = Integer.valueOf("01");
+        System.out.println(i);
+    }
+
+}
