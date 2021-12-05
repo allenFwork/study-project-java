e3973c4fd4030049063f4fa6cb9aa385b9eafd9e
allenFwork <1042160867@qq.com>
2021-12-05 10:40:32 +0800

    数据库迁移优化版本 12-05

---
+++ data-migration-study/data-migration-sqlserver/src/main/java/com/study/SqlServerApplicationStartUpdate.java
@@ -0,0 +1,172 @@
+package com.study;
+
+import com.study.util.DataBaseConnectPool;
+import com.study.util.DataBaseConnectPool2;
+import com.study.util.DateUtil;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.Date;
+import java.util.List;
+import java.util.Vector;
+import java.util.concurrent.*;
+
+public class SqlServerApplicationStartUpdate {
+
+    static DataBaseConnectPool dataBaseConnectPool1 = new DataBaseConnectPool();
+    static DataBaseConnectPool2 dataBaseConnectPool2 = new DataBaseConnectPool2();
+
+    public static void main(String[] args) {
+
+        // 数据库中总记录数
+        long rows = 27854644L;
+//        long rows = 27854644L;  6985280
+
+        // 核心线程数
+        int kThreads = Runtime.getRuntime().availableProcessors() * 50;
+        // 任务数(这里需要求一下平均每个任务需要执行的任务id大小是多少，实际测试中，20-30w 快则 2秒，慢则3-5秒，这个阈值是比较理想的 也就是 总记录数/任意数 等到想要的平均任务数)
+//        Long pageSize = (rows / 8000) + 1;
+        Long pageSize = rows / (kThreads - 1);
+
+        // 线程池
+        ExecutorService threadPool = Executors.newFixedThreadPool(kThreads);
+
+        // 数据起始位
+        Long pageNo = 0L;
+
+        System.out.println("开始时间：" + DateUtil.toDateString(new Date(), DateUtil.DATE_PATTERN2));
+        // 存放线程执行结果
+        List<Future> list = new Vector<>();
+        // 执行多少次任务 = 结束位置不小于总记录数
+        for (Long i = 0l; i < rows; i = pageNo) {
+            Connection connection1 = dataBaseConnectPool1.getConnection();
+            Connection connection2 = dataBaseConnectPool2.getConnection();
+            final Future submit = threadPool.submit(new DataThread(pageNo, pageSize, connection1, connection2));
+            // 结束id=开始id+任务数
+            pageNo = pageNo + pageSize;
+            list.add(submit);
+        }
+        // 打印结果
+        list.forEach(dx -> {
+            try {
+                System.out.println(dx.get());
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } catch (ExecutionException e) {
+                e.printStackTrace();
+            }
+        });
+        System.out.println("结束时间：" + DateUtil.toDateString(new Date(), DateUtil.DATE_PATTERN2));
+    }
+
+    // 线程操作资源类
+    static class DataThread implements Callable {
+
+        private Connection connection1;
+        private Connection connection2;
+        private Long pageNo, pageSize;
+
+        public DataThread(Long pageNo, Long pageSize, Connection connection1, Connection connection2) {
+            this.connection1 = connection1;
+            this.connection2 = connection2;
+            this.pageNo = pageNo;
+            this.pageSize = pageSize;
+            System.out.println("pageNo:" + pageNo + ",pageSize:" + pageSize);
+        }
+
+        @Override
+        public Object call() throws Exception {
+            connection1.setAutoCommit(true);
+            connection2.setAutoCommit(true);
+            try {
+                System.out.println("[起始页码: " + pageNo + ", 结束页码: " + (pageNo + pageSize) + "] 开始执行 ... ");
+                // 起始页码
+                long pageNoByCount = pageNo;
+                // 每页查询数量
+                long pageSizeByCount = pageSize / 10000;
+                // 剩余数量
+                long leaveCount = pageSize % 10000;
+                String sql1 = "SELECT * FROM Tactical_Offline..sc_ship_part_cml_copy ORDER BY BU OFFSET ? rows fetch next ? rows only";
+                String sql2 = "INSERT INTO HAWK_TACTICAL..sc_ship_part_cml_copy (bu,product_group,brand,plant_code,geo,subgeo,site_id,family,product,product_qty,sbb,sbb_qty,part,part_qty,ship_date,sys_created_by,update_timestamp,itemgroup,sci_site_id,product_line,ludp_update_timestamp,segment) " +
+                        "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+                PreparedStatement preparedStatement1 = null;
+                PreparedStatement preparedStatement2 = null;
+                for (int i = 0; i < pageSizeByCount; i++) {
+                    preparedStatement1 = connection1.prepareStatement(sql1);
+                    preparedStatement1.setLong(1, pageNoByCount);
+                    preparedStatement1.setLong(2, 10000);
+                    ResultSet resultSet = preparedStatement1.executeQuery();
+                    preparedStatement2 = connection2.prepareStatement(sql2);
+                    while (resultSet.next()) {
+                        preparedStatement2.setString(1, resultSet.getString(1));
+                        preparedStatement2.setString(2, resultSet.getString(2));
+                        preparedStatement2.setString(3, resultSet.getString(3));
+                        preparedStatement2.setString(4, resultSet.getString(4));
+                        preparedStatement2.setString(5, resultSet.getString(5));
+                        preparedStatement2.setString(6, resultSet.getString(6));
+                        preparedStatement2.setString(7, resultSet.getString(7));
+                        preparedStatement2.setString(8, resultSet.getString(8));
+                        preparedStatement2.setString(9, resultSet.getString(9));
+                        preparedStatement2.setFloat(10, resultSet.getFloat(10));
+                        preparedStatement2.setString(11, resultSet.getString(11));
+                        preparedStatement2.setFloat(12, resultSet.getFloat(12));
+                        preparedStatement2.setString(13, resultSet.getString(13));
+                        preparedStatement2.setFloat(14, resultSet.getFloat(14));
+                        preparedStatement2.setString(15, resultSet.getString(15));
+                        preparedStatement2.setString(16, resultSet.getString(16));
+                        preparedStatement2.setString(17, resultSet.getString(17));
+                        preparedStatement2.setString(18, resultSet.getString(18));
+                        preparedStatement2.setString(19, resultSet.getString(19));
+                        preparedStatement2.setString(20, resultSet.getString(20));
+                        preparedStatement2.setString(21, resultSet.getString(21));
+                        preparedStatement2.setString(22, resultSet.getString(22));
+                        preparedStatement2.addBatch();
+                    }
+                    preparedStatement2.executeBatch();
+                    pageNoByCount = pageNoByCount + 10000;
+                }
+                if (leaveCount > 0) {
+                    preparedStatement1 = connection1.prepareStatement(sql1);
+                    preparedStatement1.setLong(1, pageNoByCount);
+                    preparedStatement1.setLong(2, leaveCount);
+                    ResultSet resultSet = preparedStatement1.executeQuery();
+                    preparedStatement2 = connection2.prepareStatement(sql2);
+                    while (resultSet.next()) {
+                        preparedStatement2.setString(1, resultSet.getString(1));
+                        preparedStatement2.setString(2, resultSet.getString(2));
+                        preparedStatement2.setString(3, resultSet.getString(3));
+                        preparedStatement2.setString(4, resultSet.getString(4));
+                        preparedStatement2.setString(5, resultSet.getString(5));
+                        preparedStatement2.setString(6, resultSet.getString(6));
+                        preparedStatement2.setString(7, resultSet.getString(7));
+                        preparedStatement2.setString(8, resultSet.getString(8));
+                        preparedStatement2.setString(9, resultSet.getString(9));
+                        preparedStatement2.setFloat(10, resultSet.getFloat(10));
+                        preparedStatement2.setString(11, resultSet.getString(11));
+                        preparedStatement2.setFloat(12, resultSet.getFloat(12));
+                        preparedStatement2.setString(13, resultSet.getString(13));
+                        preparedStatement2.setFloat(14, resultSet.getFloat(14));
+                        preparedStatement2.setString(15, resultSet.getString(15));
+                        preparedStatement2.setString(16, resultSet.getString(16));
+                        preparedStatement2.setString(17, resultSet.getString(17));
+                        preparedStatement2.setString(18, resultSet.getString(18));
+                        preparedStatement2.setString(19, resultSet.getString(19));
+                        preparedStatement2.setString(20, resultSet.getString(20));
+                        preparedStatement2.setString(21, resultSet.getString(21));
+                        preparedStatement2.setString(22, resultSet.getString(22));
+                        preparedStatement2.addBatch();
+                    }
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+                return "[起始页码: " + pageNo + ", 结束页码: " + (pageNo + pageSize) + "] 执行失败! ";
+            } finally {
+                dataBaseConnectPool1.releaseConnection(connection1);
+                dataBaseConnectPool2.releaseConnection(connection2);
+            }
+            return "[起始页码: " + pageNo + ", 结束页码: " + (pageNo + pageSize) + "] 执行成功! ";
+        }
+    }
+
+}
