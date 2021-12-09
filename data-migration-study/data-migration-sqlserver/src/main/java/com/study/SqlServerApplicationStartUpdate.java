7a230dd0bf01c6f974bbea48473de380338fc182
allenwork <allenwork2021@163.com>
2021-12-09 09:22:57 +0800

    千万级别数据迁移 2021-12-09

--- data-migration-study/data-migration-sqlserver/src/main/java/com/study/SqlServerApplicationStartUpdate.java
+++ data-migration-study/data-migration-sqlserver/src/main/java/com/study/SqlServerApplicationStartUpdate.java
@@ -21,7 +21,7 @@ public class SqlServerApplicationStartUpdate {
     public static void main(String[] args) {
 
         // 数据库中总记录数
-        long rows = 1541060L;
+        long rows = 4713347L;
 
         // 核心线程数
         int kThreads = Runtime.getRuntime().availableProcessors() * 10; // 80
@@ -113,9 +113,15 @@ public class SqlServerApplicationStartUpdate {
 //                String sql1 = "SELECT * FROM Tactical_Offline..sc_ship_part_cml_copy44 ORDER BY BU OFFSET ? rows fetch next ? rows only";
 //                String sql2 = "INSERT INTO HAWK_TACTICAL..sc_ship_part_cml (bu,product_group,brand,plant_code,geo,subgeo,site_id,family,product,product_qty,sbb,sbb_qty,part,part_qty,ship_date,sys_created_by,update_timestamp,itemgroup,sci_site_id,product_line,ludp_update_timestamp,segment) " +
 //                        "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
-                String sql1 = "SELECT * FROM Tactical_Offline..sc_dim_product_rt_cml_copy8 ORDER BY product OFFSET ? rows fetch next ? rows only";
-                String sql2 = "INSERT INTO HAWK_TACTICAL..sc_dim_product_rt_cml (product,maktx,extwg,brand,product_group,business_unit,prodh,ph1_desc,ph2_desc,ph3_desc,ph4_desc,matkl,sys_creation_date,product_family,mvgr3,effstartdate,effenddate,ph5_desc,ph6_desc,ph1,ph2,ph3,ph4,ph5,ph6,mbg_family) " +
-                        "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+
+//                String sql1 = "SELECT * FROM Tactical_Offline..sc_dim_product_rt_cml_copy8 ORDER BY product OFFSET ? rows fetch next ? rows only";
+//                String sql2 = "INSERT INTO HAWK_TACTICAL..sc_dim_product_rt_cml (product,maktx,extwg,brand,product_group,business_unit,prodh,ph1_desc,ph2_desc,ph3_desc,ph4_desc,matkl,sys_creation_date,product_family,mvgr3,effstartdate,effenddate,ph5_desc,ph6_desc,ph1,ph2,ph3,ph4,ph5,ph6,mbg_family) " +
+//                        "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+
+                String sql1 = "SELECT material, bu, commodity, itemgroup, leadtime, liabilitywindow, material_category, sys_creation_date, sys_last_modified_date, sys_created_by, sys_last_modified_by, site_code, commodity_source FROM Tactical_Offline..sc_parts_attribute_fact_rt_cml ORDER BY id OFFSET ? rows fetch next ? rows only";
+                String sql2 = "INSERT INTO HAWK_TACTICAL..sc_parts_attribute_fact_rt_cml (material, bu, commodity, itemgroup, leadtime, liabilitywindow, material_category, sys_creation_date, sys_last_modified_date, sys_created_by, sys_last_modified_by, site_code, commodity_source) " +
+                        "values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+
                 PreparedStatement preparedStatement1 = null;
                 PreparedStatement preparedStatement2 = null;
                 for (int i = 0; i < pageSizeByCount; i++) {
@@ -149,12 +155,40 @@ public class SqlServerApplicationStartUpdate {
 //                        preparedStatement2.setString(22, resultSet.getString(22));
 //                        preparedStatement2.addBatch();
 
+//                        preparedStatement2.setString(1, resultSet.getString(1));
+//                        preparedStatement2.setString(2, resultSet.getString(2));
+//                        preparedStatement2.setString(3, resultSet.getString(3));
+//                        preparedStatement2.setString(4, resultSet.getString(4));
+//                        preparedStatement2.setString(5, resultSet.getString(5));
+//                        preparedStatement2.setString(6, resultSet.getString(6));
+//                        preparedStatement2.setString(7, resultSet.getString(7));
+//                        preparedStatement2.setString(8, resultSet.getString(8));
+//                        preparedStatement2.setString(9, resultSet.getString(9));
+//                        preparedStatement2.setString(10, resultSet.getString(10));
+//                        preparedStatement2.setString(11, resultSet.getString(11));
+//                        preparedStatement2.setString(12, resultSet.getString(12));
+//                        preparedStatement2.setString(13, resultSet.getString(13));
+//                        preparedStatement2.setString(14, resultSet.getString(14));
+//                        preparedStatement2.setString(15, resultSet.getString(15));
+//                        preparedStatement2.setString(16, resultSet.getString(16));
+//                        preparedStatement2.setString(17, resultSet.getString(17));
+//                        preparedStatement2.setString(18, resultSet.getString(18));
+//                        preparedStatement2.setString(19, resultSet.getString(19));
+//                        preparedStatement2.setString(20, resultSet.getString(20));
+//                        preparedStatement2.setString(21, resultSet.getString(21));
+//                        preparedStatement2.setString(22, resultSet.getString(22));
+//                        preparedStatement2.setString(23, resultSet.getString(23));
+//                        preparedStatement2.setString(24, resultSet.getString(24));
+//                        preparedStatement2.setString(25, resultSet.getString(25));
+//                        preparedStatement2.setString(26, resultSet.getString(26));
+//                        preparedStatement2.addBatch();
+
                         preparedStatement2.setString(1, resultSet.getString(1));
                         preparedStatement2.setString(2, resultSet.getString(2));
                         preparedStatement2.setString(3, resultSet.getString(3));
                         preparedStatement2.setString(4, resultSet.getString(4));
-                        preparedStatement2.setString(5, resultSet.getString(5));
-                        preparedStatement2.setString(6, resultSet.getString(6));
+                        preparedStatement2.setFloat(5, resultSet.getFloat(5));
+                        preparedStatement2.setFloat(6, resultSet.getFloat(6));
                         preparedStatement2.setString(7, resultSet.getString(7));
                         preparedStatement2.setString(8, resultSet.getString(8));
                         preparedStatement2.setString(9, resultSet.getString(9));
@@ -162,19 +196,6 @@ public class SqlServerApplicationStartUpdate {
                         preparedStatement2.setString(11, resultSet.getString(11));
                         preparedStatement2.setString(12, resultSet.getString(12));
                         preparedStatement2.setString(13, resultSet.getString(13));
-                        preparedStatement2.setString(14, resultSet.getString(14));
-                        preparedStatement2.setString(15, resultSet.getString(15));
-                        preparedStatement2.setString(16, resultSet.getString(16));
-                        preparedStatement2.setString(17, resultSet.getString(17));
-                        preparedStatement2.setString(18, resultSet.getString(18));
-                        preparedStatement2.setString(19, resultSet.getString(19));
-                        preparedStatement2.setString(20, resultSet.getString(20));
-                        preparedStatement2.setString(21, resultSet.getString(21));
-                        preparedStatement2.setString(22, resultSet.getString(22));
-                        preparedStatement2.setString(23, resultSet.getString(23));
-                        preparedStatement2.setString(24, resultSet.getString(24));
-                        preparedStatement2.setString(25, resultSet.getString(25));
-                        preparedStatement2.setString(26, resultSet.getString(26));
                         preparedStatement2.addBatch();
 
                     }
@@ -214,12 +235,40 @@ public class SqlServerApplicationStartUpdate {
 //                        preparedStatement2.setString(22, resultSet.getString(22));
 //                        preparedStatement2.addBatch();
 
+//                        preparedStatement2.setString(1, resultSet.getString(1));
+//                        preparedStatement2.setString(2, resultSet.getString(2));
+//                        preparedStatement2.setString(3, resultSet.getString(3));
+//                        preparedStatement2.setString(4, resultSet.getString(4));
+//                        preparedStatement2.setString(5, resultSet.getString(5));
+//                        preparedStatement2.setString(6, resultSet.getString(6));
+//                        preparedStatement2.setString(7, resultSet.getString(7));
+//                        preparedStatement2.setString(8, resultSet.getString(8));
+//                        preparedStatement2.setString(9, resultSet.getString(9));
+//                        preparedStatement2.setString(10, resultSet.getString(10));
+//                        preparedStatement2.setString(11, resultSet.getString(11));
+//                        preparedStatement2.setString(12, resultSet.getString(12));
+//                        preparedStatement2.setString(13, resultSet.getString(13));
+//                        preparedStatement2.setString(14, resultSet.getString(14));
+//                        preparedStatement2.setString(15, resultSet.getString(15));
+//                        preparedStatement2.setString(16, resultSet.getString(16));
+//                        preparedStatement2.setString(17, resultSet.getString(17));
+//                        preparedStatement2.setString(18, resultSet.getString(18));
+//                        preparedStatement2.setString(19, resultSet.getString(19));
+//                        preparedStatement2.setString(20, resultSet.getString(20));
+//                        preparedStatement2.setString(21, resultSet.getString(21));
+//                        preparedStatement2.setString(22, resultSet.getString(22));
+//                        preparedStatement2.setString(23, resultSet.getString(23));
+//                        preparedStatement2.setString(24, resultSet.getString(24));
+//                        preparedStatement2.setString(25, resultSet.getString(25));
+//                        preparedStatement2.setString(26, resultSet.getString(26));
+//                        preparedStatement2.addBatch();
+
                         preparedStatement2.setString(1, resultSet.getString(1));
                         preparedStatement2.setString(2, resultSet.getString(2));
                         preparedStatement2.setString(3, resultSet.getString(3));
                         preparedStatement2.setString(4, resultSet.getString(4));
-                        preparedStatement2.setString(5, resultSet.getString(5));
-                        preparedStatement2.setString(6, resultSet.getString(6));
+                        preparedStatement2.setFloat(5, resultSet.getFloat(5));
+                        preparedStatement2.setFloat(6, resultSet.getFloat(6));
                         preparedStatement2.setString(7, resultSet.getString(7));
                         preparedStatement2.setString(8, resultSet.getString(8));
                         preparedStatement2.setString(9, resultSet.getString(9));
@@ -227,19 +276,6 @@ public class SqlServerApplicationStartUpdate {
                         preparedStatement2.setString(11, resultSet.getString(11));
                         preparedStatement2.setString(12, resultSet.getString(12));
                         preparedStatement2.setString(13, resultSet.getString(13));
-                        preparedStatement2.setString(14, resultSet.getString(14));
-                        preparedStatement2.setString(15, resultSet.getString(15));
-                        preparedStatement2.setString(16, resultSet.getString(16));
-                        preparedStatement2.setString(17, resultSet.getString(17));
-                        preparedStatement2.setString(18, resultSet.getString(18));
-                        preparedStatement2.setString(19, resultSet.getString(19));
-                        preparedStatement2.setString(20, resultSet.getString(20));
-                        preparedStatement2.setString(21, resultSet.getString(21));
-                        preparedStatement2.setString(22, resultSet.getString(22));
-                        preparedStatement2.setString(23, resultSet.getString(23));
-                        preparedStatement2.setString(24, resultSet.getString(24));
-                        preparedStatement2.setString(25, resultSet.getString(25));
-                        preparedStatement2.setString(26, resultSet.getString(26));
                         preparedStatement2.addBatch();
                     }
                     preparedStatement2.executeBatch();
