2ffd40c202d857d334bc169f95335878d74f9f70
allenFwork <1042160867@qq.com>
2024-01-16 00:14:10 +0800

    springCloud学习：以nacos为注册中心学习使用

--- springcloud-study/springcloud-parent-01/ms-user-service/src/main/java/com/study/springcloud/user/web/UserController.java
+++ springcloud-study/springcloud-parent-01/ms-user-service/src/main/java/com/study/springcloud/user/web/UserController.java
@@ -1,11 +1,18 @@
 package com.study.springcloud.user.web;
 
+import com.study.springcloud.user.config.PatternProperties;
 import com.study.springcloud.user.pojo.User;
 import com.study.springcloud.user.service.UserService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+@RefreshScope
 @Slf4j
 @RestController
 @RequestMapping("/user")
@@ -13,6 +20,19 @@ public class UserController {
 
     @Autowired
     private UserService userService;
+
+    @Value("${pattern.dateformat}")
+    private String dateformat;
+
+    @Autowired
+    private PatternProperties patternProperties;
+
+    @GetMapping("now")
+    public String now() {
+        System.out.println("从nacos服务器上读取的配置信息：pattern.dateformat: " + dateformat);
+        System.out.println("从nacos服务器上读取的配置信息：patternProperties的pattern.dateformat: " + patternProperties.getDateformat());
+        return LocalDateTime.now().format(DateTimeFormatter.ofPattern(dateformat));
+    }
 
     /**
      * 路径： /user/110
