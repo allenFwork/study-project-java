cf28ac9c0b0a440afb93c681a84ed29eb8dc684a
allenFwork <shiwei@lenovo.com>
2024-04-01 17:09:11 +0800

    JVM学习：局部变量表相关学习

---
+++ jvm-study/jvm-classloader-demo/src/main/java/com/study/jvm/local_variable_table/LocalVariableTableDemo.java
@@ -0,0 +1,48 @@
+package com.study.jvm.local_variable_table;
+
+/**
+ * 栈帧-局部变量表
+ */
+public class LocalVariableTableDemo {
+    public static void test1() {
+        int i = 0;
+        long j = 1;
+    }
+
+    public void test2() {
+        int i = 0;
+        long j = 1;
+    }
+
+    public void test3(int k, int m) {
+        int i = 0;
+        long j = 1;
+    }
+
+    public void test4(int k, int m) {
+        {
+            int a = 1;
+            int b = 2;
+        }
+        {
+            int c = 1;
+        }
+        int i = 0;
+        long j = 1;
+    }
+
+    public void test5() {
+        int i = 0;
+        int j = i + 1;
+    }
+
+    public static void test6() {
+        int i = 0;
+        try {
+            i = 1;
+        } catch (Exception e) {
+            i = 2;
+        }
+
+    }
+}
