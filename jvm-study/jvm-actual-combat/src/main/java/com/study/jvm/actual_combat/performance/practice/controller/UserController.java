0c05ad55e8152d843050a21435314de169ce27fc
allenFwork <shiwei@lenovo.com>
2024-04-15 14:27:54 +0800

    JVM学习：实战调优

--- jvm-study/jvm-actual-combat/src/main/java/com/study/jvm/actual_combat/performance/practice/controller/UserController.java
+++ jvm-study/jvm-actual-combat/src/main/java/com/study/jvm/actual_combat/performance/practice/controller/UserController.java
@@ -27,29 +27,29 @@ public class UserController {
 
     private final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
 
-    //初始代码
-    public List<UserVO> user1(){
-        //1.从数据库获取前端需要的详情数据
+    // 初始代码
+    public List<UserVO> user1() {
+        // 1.从数据库获取前端需要的详情数据
         List<UserDetails> userDetails = userService.getUserDetails();
 
-        //2.获取缓存中的用户数据
+        // 2.获取缓存中的用户数据
         List<User> users = userService.getUsers();
 
         SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-        //3.遍历详情集合，从缓存中获取用户名，生成VO进行填充
+        // 3.遍历详情集合，从缓存中获取用户名，生成VO进行填充
         ArrayList<UserVO> userVOS = new ArrayList<>();
         for (UserDetails userDetail : userDetails) {
             UserVO userVO = new UserVO();
-            //可以使用BeanUtils对象拷贝
+            // 可以使用BeanUtils对象拷贝
             userVO.setId(userDetail.getId());
             userVO.setRegister(simpleDateFormat.format(userDetail.getRegister2()));
-            //填充name
+            // 填充name
             for (User user : users) {
-                if(user.getId().equals(userDetail.getId())){
+                if (user.getId().equals(userDetail.getId())) {
                     userVO.setName(user.getName());
                 }
             }
-            //加入集合
+            // 加入集合
             userVOS.add(userVO);
         }
 
@@ -59,7 +59,7 @@ public class UserController {
 
 
     //使用HasmMap存放用户名字
-    public List<UserVO> user2(){
+    public List<UserVO> user2() {
         //1.从数据库获取前端需要的详情数据
         List<UserDetails> userDetails = userService.getUserDetails();
 
@@ -68,7 +68,7 @@ public class UserController {
         //将list转换成hashmap
         HashMap<Long, User> map = new HashMap<>();
         for (User user : users) {
-            map.put(user.getId(),user);
+            map.put(user.getId(), user);
         }
 
         SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
@@ -91,7 +91,7 @@ public class UserController {
 
 
     //优化日期格式化
-    public List<UserVO> user3(){
+    public List<UserVO> user3() {
         //1.从数据库获取前端需要的详情数据
         List<UserDetails> userDetails = userService.getUserDetails();
 
@@ -100,7 +100,7 @@ public class UserController {
         //将list转换成hashmap
         HashMap<Long, User> map = new HashMap<>();
         for (User user : users) {
-            map.put(user.getId(),user);
+            map.put(user.getId(), user);
         }
 
         //3.遍历详情集合，从缓存中获取用户名，生成VO进行填充
@@ -122,7 +122,7 @@ public class UserController {
 
     @GetMapping
     //使用stream流改写for循环
-    public List<UserVO> user4(){
+    public List<UserVO> user4() {
         //1.从数据库获取前端需要的详情数据
         List<UserDetails> userDetails = userService.getUserDetails();
 
@@ -145,7 +145,7 @@ public class UserController {
     }
 
     //使用并行流优化性能
-    public List<UserVO> user5(){
+    public List<UserVO> user5() {
         //1.从数据库获取前端需要的详情数据
         List<UserDetails> userDetails = userService.getUserDetails();
 
