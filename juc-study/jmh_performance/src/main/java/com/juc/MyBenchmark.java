7d31213604643acb5fdbb771aa24ac69febddb8c
allenFwork <shiwei@lenovo.com>
2024-04-29 13:43:54 +0800

    juc学习：通过jmh测试多核CPU情况下多线程并发线程

--- juc-study/jmh_performance/src/main/java/com/juc/MyBenchmark.java
+++ juc-study/jmh_performance/src/main/java/com/juc/MyBenchmark.java
@@ -23,7 +23,7 @@
  * questions.
  */
 
-package com.itcast;
+package com.juc;
 
 import org.openjdk.jmh.annotations.*;
 
@@ -31,42 +31,44 @@ import java.util.Arrays;
 import java.util.concurrent.FutureTask;
 
 @Fork(1)
-@BenchmarkMode(Mode.AverageTime)
-@Warmup(iterations=3)
-@Measurement(iterations=5)
+@BenchmarkMode(Mode.AverageTime) // 统计平均时间
+@Warmup(iterations = 3)
+@Measurement(iterations = 5)
 public class MyBenchmark {
     static int[] ARRAY = new int[1000_000_00];
+
     static {
         Arrays.fill(ARRAY, 1);
     }
+
     @Benchmark
     public int c() throws Exception {
         int[] array = ARRAY;
-        FutureTask<Integer> t1 = new FutureTask<>(()->{
+        FutureTask<Integer> t1 = new FutureTask<>(() -> {
             int sum = 0;
-            for(int i = 0; i < 250_000_00;i++) {
-                sum += array[0+i];
+            for (int i = 0; i < 250_000_00; i++) {
+                sum += array[0 + i];
             }
             return sum;
         });
-        FutureTask<Integer> t2 = new FutureTask<>(()->{
+        FutureTask<Integer> t2 = new FutureTask<>(() -> {
             int sum = 0;
-            for(int i = 0; i < 250_000_00;i++) {
-                sum += array[250_000_00+i];
+            for (int i = 0; i < 250_000_00; i++) {
+                sum += array[250_000_00 + i];
             }
             return sum;
         });
-        FutureTask<Integer> t3 = new FutureTask<>(()->{
+        FutureTask<Integer> t3 = new FutureTask<>(() -> {
             int sum = 0;
-            for(int i = 0; i < 250_000_00;i++) {
-                sum += array[500_000_00+i];
+            for (int i = 0; i < 250_000_00; i++) {
+                sum += array[500_000_00 + i];
             }
             return sum;
         });
-        FutureTask<Integer> t4 = new FutureTask<>(()->{
+        FutureTask<Integer> t4 = new FutureTask<>(() -> {
             int sum = 0;
-            for(int i = 0; i < 250_000_00;i++) {
-                sum += array[750_000_00+i];
+            for (int i = 0; i < 250_000_00; i++) {
+                sum += array[750_000_00 + i];
             }
             return sum;
         });
@@ -74,15 +76,16 @@ public class MyBenchmark {
         new Thread(t2).start();
         new Thread(t3).start();
         new Thread(t4).start();
-        return t1.get() + t2.get() + t3.get()+ t4.get();
+        return t1.get() + t2.get() + t3.get() + t4.get();
     }
+
     @Benchmark
     public int d() throws Exception {
         int[] array = ARRAY;
-        FutureTask<Integer> t1 = new FutureTask<>(()->{
+        FutureTask<Integer> t1 = new FutureTask<>(() -> {
             int sum = 0;
-            for(int i = 0; i < 1000_000_00;i++) {
-                sum += array[0+i];
+            for (int i = 0; i < 1000_000_00; i++) {
+                sum += array[0 + i];
             }
             return sum;
         });
