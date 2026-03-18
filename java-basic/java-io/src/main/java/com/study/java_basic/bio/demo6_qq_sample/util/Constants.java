e6d12cd9cdb3036b950d061875ee83d6cc61a75e
allenFwork <1042160867@qq.com>
2026-03-18 16:27:54 +0800

    Java的IO模型学习案例：基于BIO实现实时消息聊天（模拟QQ）

---
+++ java-basic/java-io/src/main/java/com/study/java_basic/bio/demo6_qq_sample/util/Constants.java
@@ -0,0 +1,9 @@
+package com.study.java_basic.bio.demo6_qq_sample.util;
+
+public class Constants {
+	/** 常量 */
+	public static final int PORT = 7778 ;
+	
+	/** 协议分隔符 */
+	public static final String SPLIT = "003197♣♣㏘♣④④♣";
+}
